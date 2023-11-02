a81dc6c3742df27ac8035488d10ed49ffcc847d1
big-charter <bigcharter@protonmail.com>
2023-11-01 21:45:39 -0400

    Start over:)

--- hello.hs
+++ hello.hs
@@ -1,16 +1,3 @@
--- hello.hs
-
 main = putStrLn myhtml
 
-myhtml = makeHtml "Hello title" "Hello, world!"
-
-makeHtml title content = html_ (head_ (title_ title) <> body_ content)
-
-html_ content = "<html>" <> content <> "</html>"
-   
-body_ content = "<body>" <> content <> "</body>"
-
-head_ content = "<head>" <> content <> "</head>"
-
-title_ content = "<title>" <> content <> "</title>"
-
+myhtml = "<html><body>Hello, world!</body></html>"
